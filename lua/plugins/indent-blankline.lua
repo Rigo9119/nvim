ac19d0dd97fb4ae55359f13350b4261290e5cbe6
Rigo Rosero C <rigo4791@gmail.com>
2023-12-30 19:10:46 -0500

    removes indent rainbow and set ups indentBlankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,13 +1,16 @@
+local highlight = {
+  "CursorColumn",
+  "Whitespace",
+}
+
 return {
   "lukas-reineke/indent-blankline.nvim",
-  opts = function(_, opts)
-    -- Other blankline configuration here
-    return require("indent-rainbowline").make_opts(opts, {
-      color_transparency = 0.8,
-      colors = { 0x610C9F, 0x940B92, 0xDA0C81, 0xE95793 },
-    })
-  end,
-  dependencies = {
-    "TheGLander/indent-rainbowline.nvim",
+  opts = {
+    indent = { highlight = highlight, char = "" },
+    whitespace = {
+      highlight = highlight,
+      remove_blankline_trail = false,
+    },
+    scope = { enabled = false },
   },
 }
