460b758ab13479fc2f3a2aa2f3064286a1262cb2
Rigo Rosero C <rigo4791@gmail.com>
2024-01-02 18:38:00 -0500

    adds rule to neo tree to show hidden files
    - adds neo-tree.lua file to the project
    - adds neo tree config to show hidden files

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- bootstrap lazy.nvim, azyVim and your plugins
 require("config.lazy")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
