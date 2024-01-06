3d7a88eb4f16a497044fd2a0ac495b8505fd8533
Rigo Rosero C <rigo4791@gmail.com>
2024-01-06 12:29:16 -0500

    add everforest and pine rose themes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
-      colorscheme = "tokyonight",
+      colorscheme = "neo-forest",
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
