e703b7c717c9fab9d8db20a069d202c050b20f72
Rigo Rosero Castillo <rigo4791@gmail.com>
2024-12-01 21:43:31 -0500

    fixes lualine loading on start

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -68,37 +68,42 @@ local fileFormat = {
   color = { fg = colors.white, gui = "bold" },
 }
 
-require("lualine").setup({
-  options = {
-    theme = bubbles_theme,
-    component_separators = "",
-    section_separators = { left = "", right = "" },
-  },
-  sections = {
-    lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-    lualine_b = {
-      "filename",
-      "branch",
-      diff,
-      diagnostics,
-    },
-    lualine_c = {
-      "%=", --[[ add your center compoentnts here in place of this comment ]]
-    },
-    lualine_x = {},
-    lualine_y = { encoding, fileFormat, "filetype", "progress" },
-    lualine_z = {
-      { "location", separator = { right = "" }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { "filename" },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { "location" },
-  },
-  tabline = {},
-  extensions = {},
-})
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = bubbles_theme,
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = {
+          "filename",
+          "branch",
+          diff,
+          diagnostics,
+        },
+        lualine_c = {
+          "%=", --[[ add your center compoentnts here in place of this comment ]]
+        },
+        lualine_x = {},
+        lualine_y = { encoding, fileFormat, "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      tabline = {},
+      extensions = {},
+    })
+  end,
+}
